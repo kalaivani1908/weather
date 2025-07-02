7d99f4d9215eb638e61b1fb049e01c2d0dfa56f2
kalaivani1908 <sivasundari2004@gmail.com>
2025-07-02 16:54:51 +0530

    Update main2.dart

--- main2.dart
+++ main2.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/screens/loading_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Weather App',
       theme: ThemeData.dark(),
-      home: LoadingScreen(),
+      home: const LoadingScreen(),
     );
   }
 }
